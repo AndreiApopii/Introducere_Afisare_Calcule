bb03e929137d170d7d0eafcbb70e44ed1f46f7ff
AndreiApopii <70907129+AndreiApopii@users.noreply.github.com>
2020-10-07 16:13:49 +0300

    Add files via upload

---
+++ problema5.py
@@ -0,0 +1,3 @@
+nr=int(input("dati un numar"))
+if nr=5
+print(",nr-2," ",nr-1," ",nr," ",nr+1," ",nr+2,")
---
+++ problema6.py
@@ -0,0 +1,4 @@
+n=int(input("dati nr de mere primite de copii"))
+print("primul copil are: ",n-2," mere")
+print("al doilea copil are: ",n+1," mere")
+n==10
