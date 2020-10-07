d6db1b77ae13f814213dd32892e8a5e24e67483a
AndreiApopii <70907129+AndreiApopii@users.noreply.github.com>
2020-10-07 15:56:25 +0300

    Add files via upload

--- problema1.py
+++ problema1.py
@@ -1,4 +1,18 @@
-nf=int(input("dati numarul de fete"))
-nb=nf+10
-nt=nf+nb
-print("in tabara sunt ",nt," copii")
+n=int(input("dati numarul culorii"))
+if n==1:
+    print("rosu")
+if n==2:
+    print("oranj")
+if n==3:
+    print("galben")
+if n==4:
+    print("verde")
+if n==5:
+    print("albastru")
+if n==6:
+    print("indigo")
+if n==7:
+    print("violet")
+if((n<7)or(n>1)):
+    print("curcubeul are sapte culori")
+
